bc5168fa74f42f66e559311ce93d5abd282de108
Christoph Rust <christoph.g.rust@gmail.com>
2022-09-30 22:15:51 +0200

    add main function prototype

---
+++ R/dpca.R
@@ -0,0 +1,9 @@
+#' Dynamic Principal Component Analysis and Large Dynamic Factor Models
+#'
+#' @param x Input
+#' @useDynLib dpca
+dpca <- function(x,...) {
+
+  res <- list()
+  class(res) <- "dpca"
+}
