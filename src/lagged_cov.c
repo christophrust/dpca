af4311309e126064a94b0985282f199e05f6a574
Christoph Rust <christoph.g.rust@gmail.com>
2022-10-18 11:29:17 +0200

    update compile commmands + lagged_covs

--- src/lagged_cov.c
+++ src/lagged_cov.c
@@ -1,9 +1,9 @@
-#include "R_ext/Error.h"
+#include <R.h>
+#include "Rinternals.h"
 #include "R_ext/RS.h"
 #include <R_ext/Lapack.h>
-#include "Rinternals.h"
+#include "R_ext/Error.h"
 #include <math.h>
-#include <stdlib.h>
 #include "dpca.h"
 
 void lagged_cov(double *x, double *y, double *res, int lag, int nrx, int ncx, int nry, int ncy) {
@@ -25,7 +25,18 @@ void lagged_cov(double *x, double *y, double *res, int lag, int nrx, int ncx, in
 }
 
 
-// here r_x and r_y are n,m by T arrays
+
+void lagged_covs(double *x, double *y, double *res, int *lags, int nlags, int nrx, int ncx, int nry, int ncy) {
+
+    int dim = nrx * nry;
+    for (int i = 0; i < nlags; i++) {
+        lagged_cov(x, y, res + i * dim, lags[i], nrx, ncx, nry, ncy);
+    }
+
+}
+
+
+// here r_x and r_y are {n,m} by T arrays
 SEXP R_lagged_cov(SEXP r_x, SEXP r_y, SEXP r_lag, SEXP r_nrx, SEXP r_ncx, SEXP r_nry, SEXP r_ncy) {
     int ncx = *INTEGER(r_ncx);
     int nrx = *INTEGER(r_nrx);
@@ -39,11 +50,43 @@ SEXP R_lagged_cov(SEXP r_x, SEXP r_y, SEXP r_lag, SEXP r_nrx, SEXP r_ncx, SEXP r
         error("Supplied dimension of Y do not fit to passed array length!");
     if (ncx != ncy)
         error("X and Y must have same number of columns!");
+    if (ncx - 1 <= lag)
+        error("Not enough observations to compute covariance with lag %d!", lag);
 
     SEXP res = PROTECT(allocMatrix(REALSXP, nrx, nry));
 
     lagged_cov(REAL(r_x), REAL(r_y), REAL(res), lag, nrx, ncx, nry, ncy);
 
-    Rf_unprotect(1);
+    UNPROTECT(1);
+    return res;
+}
+
+
+SEXP R_lagged_covs(SEXP r_x, SEXP r_y, SEXP r_lags, SEXP r_nrx, SEXP r_ncx, SEXP r_nry, SEXP r_ncy) {
+    int ncx = *INTEGER(r_ncx);
+    int nrx = *INTEGER(r_nrx);
+    int ncy = *INTEGER(r_ncy);
+    int nry = *INTEGER(r_nry);
+    int *lags = INTEGER(r_lags);
+    int nlags = length(r_lags);
+    int maxlag = 0;
+
+    for (int i = 0; i < nlags; i++)
+        if (abs(lags[i]) > maxlag) maxlag = abs(lags[i]);
+
+    if (length(r_x) != nrx * ncx)
+        error("Supplied dimension of X do not fit to passed array length!");
+    if (length(r_y) != nry * ncy)
+        error("Supplied dimension of Y do not fit to passed array length!");
+    if (ncx != ncy)
+        error("X and Y must have same number of columns!");
+    if (ncx - 1 <= maxlag)
+        error("Not enough observations to compute covariance with maximum lag %d!", maxlag);
+
+    SEXP res = PROTECT(alloc3DArray(REALSXP, nrx, nry, nlags));
+
+    lagged_covs(REAL(r_x), REAL(r_y), REAL(res), INTEGER(r_lags), nlags, nrx, ncx, nry, ncy);
+
+    UNPROTECT(1);
     return res;
 }
--- tests/testthat/test_lagged_cov.R
+++ tests/testthat/test_lagged_cov.R
@@ -33,3 +33,81 @@ test_that("Lagged covariance estimation", {
   expect_lt(sum((res1 - res2)^2), 1e-10)
 
 })
+
+
+test_that("R_lagged_cov input checking (dimension of arrays)", {
+
+
+  ncy <- ncx <- 100L
+  nrx <- 10L
+  nry <- 15L
+
+  x <- matrix(rnorm(ncx* nrx), ncol = ncx)
+  y <- matrix(rnorm(ncy* nry), ncol = ncy)
+
+  expect_error(.Call("R_lagged_cov", x, y, 0L, nrx +1L, ncx, nry, ncy))
+  expect_error(.Call("R_lagged_cov", x, y, 0L, nrx, ncx + 1L, nry, ncy))
+  expect_error(.Call("R_lagged_cov", x, y, 0L, nrx, ncx, nry + 2L, ncy))
+  expect_error(.Call("R_lagged_cov", x, y, 0L, nrx, ncx, nry, ncy + 3L))
+
+})
+
+
+test_that("R_lagged_cov input checking (equal number of columns)", {
+
+
+  ncy <- ncx <- 100L
+  nrx <- 10L
+  nry <- 15L
+
+  x <- matrix(rnorm(ncx* nrx), ncol = ncx)
+  y <- matrix(rnorm(ncy* nry + nry), ncol = ncy + 1)
+
+  expect_error(.Call("R_lagged_cov", x, y, 0L, nrx, ncx, nry, ncy + 1L))
+
+
+})
+
+
+test_that("R_lagged_cov input checking (lag too high)", {
+
+
+  ncy <- ncx <- 100L
+  nrx <- 10L
+  nry <- 15L
+
+  x <- matrix(rnorm(ncx* nrx), ncol = ncx)
+  y <- matrix(rnorm(ncy* nry), ncol = ncy)
+
+  expect_error(.Call("R_lagged_cov", x, y, 100L, nrx, ncx, nry, ncy ))
+
+})
+
+
+## random spectrum
+test_that("Multiple lagged covariance estimation", {
+
+  ncy <- ncx <- 1000L
+  nrx <- 100L
+  nry <- 150L
+
+  x <- matrix(rnorm(ncx* nrx), ncol = ncx)
+  y <- matrix(rnorm(ncy* nry), ncol = ncy)
+
+  system.time(res1 <- .Call("R_lagged_covs", x, y, as.integer(-5:5), nrx, ncx, nry, ncy))
+  system.time(res2 <-
+                vapply(-5:5,
+                       function(l) {
+                         if (l<0)
+                           return(tcrossprod(x[,-ncx+(-l-1):0], y[,-c(1:abs(l))])/(ncx  + l))
+                         if (l == 0)
+                           return(tcrossprod(x, y)/ncx)
+
+                         tcrossprod(x[,-(1:l)], y[,-ncy + (l-1):0])/(ncx  - l)
+
+                       }, matrix(0, nrx, nry))
+              )
+
+  expect_lt(sum((res1 - res2)^2), 1e-10)
+
+})
