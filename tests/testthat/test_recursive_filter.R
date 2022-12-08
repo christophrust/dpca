0e6b9044e9318536d0d77eccb17f9defd3a9b22e
Christoph Rust <christoph.g.rust@gmail.com>
2022-12-08 16:38:55 +0100

    add unit test for recursive filter

---
+++ tests/testthat/test_recursive_filter.R
@@ -0,0 +1,22 @@
+library(dpca)
+
+test_that("Recursive filter (C-function)", {
+
+  x <- matrix(rnorm(300), nrow = 3)
+  f <- array(matrix(rnorm(18, sd = 0.1), ncol = 3), dim = c(3,3,2))
+  res1 <- .Call("R_recursive_filter", x, as.vector(f), 3L, 2L)
+  res2 <- x
+  for (i in 2:ncol(x)) {
+    if (i > 2) {
+      res2[,i] <- res2[,i] + f[,,1] %*% res2[,i-1] + f[,,2] %*% res2[,i-2]
+    } else {
+      res2[,i] <- res2[,i] + f[,,1] %*% res2[,i-1]
+    }
+  }
+
+  expect_equal(res1, res2)
+
+  res3 <- recursive_filter(x, f)
+  expect_equal(res3, res2)
+
+})
