1a62a6147fbccddbe7cdd462830fe9c2b642f588
Christoph Rust <christoph.g.rust@gmail.com>
2022-12-09 10:37:32 +0100

    add documentation of arnoldi_eigs

--- src/dpca.h
+++ src/dpca.h
@@ -69,6 +69,28 @@ void fourier_transform(double *z, int nrz, int ncz,
                        int * lags, int nlags,
                        double _Complex *res);
 
+/**
+ * @brief Compute truncated eigendecomposition of a hermitian matrix.
+ *
+ * @param mat An array of at least length ldm \times ldm holding the
+ *   matrix to be decomposed (INPUT).
+ * @param dim The dimension of the matrix mat.
+ * @param ldm Leading dimension of mat.
+ * @param q Number of eigenpairs to be computed.
+ * @param eval Array of at least lenght q which on output will contain the
+ *   computed eigenvalues.
+ * @param evecs Array of at least length dim times q which will hold the
+ *   eigenvectors on output.
+ * @param tol Tolerance passed to the ARPACK routine.
+ * @param normalize_evecs If set to a value different from zero, each of the resulting
+ *   eigenvectors is normalized such that the complex part of the first entry is zero
+ *   (the same is done in R's base::eigen()).
+ * @param verbos If different from zero, we show some messages.
+ * @param row_evecs If different from zero, the eigenvalues are roweigenvectors instead
+ *   of column-eigenvectors
+ * @param transpose_out If different from zero, the eigenvectors leading
+ *  dimension is q, otherwise ldm
+ */
 void arnoldi_eigs(Rcomplex *mat, int dim, int ldm, int q,
                   Rcomplex *eval, Rcomplex *evecs,
                   double tol, int normalize_evecs, int verbose,
