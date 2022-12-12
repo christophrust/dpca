ecbe240031456ef41681fb702a274281a9a08d2e
Christoph Rust <christoph.g.rust@gmail.com>
2022-12-12 11:21:01 +0100

    add documetation

--- src/dpca.h
+++ src/dpca.h
@@ -86,7 +86,7 @@ void fourier_transform(double *z, int nrz, int ncz,
  * @param normalize_evecs If set to a value different from zero, each of the resulting
  *   eigenvectors is normalized such that the complex part of the first entry is zero
  *   (the same is done in R's base::eigen()).
- * @param verbos If different from zero, we show some messages.
+ * @param verbose If different from zero, we show some messages.
  * @param row_evecs If different from zero, the eigenvalues are roweigenvectors instead
  *   of column-eigenvectors
  * @param transpose_out If different from zero, the eigenvectors leading
@@ -104,7 +104,26 @@ void filter_process(double *f, double *x, int *lags, int nrf,
 void complex_crossprod(double _Complex *x, int nrx, int ncx,
                        double _Complex *res, int trans_conj);
 
-int hl_select_q(double _Complex * spec, double _Complex * evals,
-            int dim, int nfreqs, int max_q, int select_q);
+/**
+ * @brief Selection of number of dynamic factors.
+ *
+ * @param spec The spectral density matrix (of dimension n by n by nfreqs).
+ * @param evals An max_q by nfreqs array which will (on output) hold in
+ * the first q by nfreqs entries the resulting eigenvalues.
+ * @param evecs An n by max_q by nfreqs array which will (on output) hold
+ * in the first n by q by nfreqs entries the resulting eigenvectors.
+ * @param dim Dimension of the spectral density matrix.
+ * @param nfreqs Number of frequencies where the spectrum is evaluated.
+ * @param max_q The maximum number of dynamic factors.
+ * @param select_q At this stage one of 1 or 2. 1 indicates that the IC1
+ * criterion is used to select q, and 2 indicates IC2.
+ * @param n_path Array holding the different values for subspectra to compute
+ * the information criteria on to do fine tuning.
+ * @param ln The length of n_path.
+ *
+ * @return The number of chosen dynamic factors (hence, q).
+ * */
+int hl_select_q(double _Complex * spec, double _Complex * evals, double _Complex *evecs,
+                int dim, int nfreqs, int max_q, int select_q, int * n_path, int ln);
 
 #endif // DPCA_H_
