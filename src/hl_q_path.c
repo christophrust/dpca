f30af3c97fbcd29b1b571aa1746584185b847f2d
Christoph Rust <christoph.g.rust@gmail.com>
2022-12-14 12:49:42 +0100

    `hl_q_path`: bugfix (initialize q = 1) + unit test

--- src/hl_q_path.c
+++ src/hl_q_path.c
@@ -12,7 +12,7 @@ void hl_q_path(double *unpenalized_ic_vals, int ln, int max_q,
     double ic, ic_temp;
 
     for (int i = 0; i < ln; i++) {
-        q = 0;
+        q = 1;
         ic = unpenalized_ic_vals[i * max_q] + penalties[i] * penalty_scale;
         for (int j = 1; j < max_q; j++) {
             ic_temp = unpenalized_ic_vals[i * max_q + j] +
---
+++ tests/testthat/test_hl_q_path.R
@@ -0,0 +1,17 @@
+library(dpca)
+
+
+contect("Hallin & Liska (2007) selection criterium")
+
+test_that("Get q path (c function)", {
+
+  max_q <- 12L
+  ln <- 10L
+  unpenalized_crit <- matrix(runif(max_q * ln, 100, 140), ncol = ln)
+  penalties <- runif(ln, min = 10, max = 12)
+  penalty_scale <- 0.001
+  r1 <- .Call("R_hl_q_path", unpenalized_crit, max_q, penalty_scale, penalties)
+  r2 <- apply(unpenalized_crit + 1:max_q %o% penalties * penalty_scale, 2, which.min)
+
+  expect_equal(r1, r2)
+})
