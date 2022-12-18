1b6610a09caddb3e26eb19450e9c6a305a5f82c1
Christoph Rust <christoph.g.rust@gmail.com>
2022-12-18 20:40:59 +0100

    change orientation of eigenvectors

--- src/hl_select_q.c
+++ src/hl_select_q.c
@@ -58,7 +58,7 @@ SEXP R_hl_select_q(SEXP r_spec, SEXP r_n_path, SEXP r_max_q, SEXP r_dim,
     int lps = length(r_penalty_scales);
 
     SEXP evals = PROTECT(allocMatrix(CPLXSXP, max_q, nfreqs));
-    SEXP evecs = PROTECT(alloc3DArray(CPLXSXP, dim, max_q, nfreqs));
+    SEXP evecs = PROTECT(alloc3DArray(CPLXSXP, max_q, dim, nfreqs));
     SEXP unpenalized_ic_vals = PROTECT(allocMatrix(REALSXP, max_q, ln));
     SEXP sample_var = PROTECT(allocVector(REALSXP, lps));
     SEXP info = PROTECT(allocVector(INTSXP, 1));
