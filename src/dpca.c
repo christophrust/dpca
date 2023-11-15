db6da51ef0e23ea613e894323908bf2a8c46818c
Christoph Rust <christoph.g.rust@gmail.com>
2023-11-15 15:50:17 +0100

    move dependency imports from header to source

--- src/dpca.c
+++ src/dpca.c
@@ -4,9 +4,35 @@
 #include "Rinternals.h"
 #include <complex.h>
 
-SEXP R_dpca(SEXP r_x, SEXP r_q, SEXP r_freqs, SEXP r_bandwidth,
-            SEXP r_tol, SEXP kernel, SEXP r_max_q, SEXP r_select_q,
-            SEXP r_n_path, SEXP r_t_path, SEXP r_penalties, SEXP r_penalty_scales) {
+#include "complex_crossprod.h"
+#include "eigs.h"
+#include "filter_process.h"
+#include "fourier_inverse.h"
+#include "fourier_transform.h"
+#include "complex_mv_product.h"
+#include "hl_find_stability_interval.h"
+#include "hl_ic.h"
+#include "hl_ic_n_path.h"
+#include "hl_q_path.h"
+#include "hl_select_q.h"
+#include "lagged_cov.h"
+
+
+SEXP R_dpca
+(
+    SEXP r_x,
+    SEXP r_q,
+    SEXP r_freqs,
+    SEXP r_bandwidth,
+    SEXP r_tol,
+    SEXP kernel,
+    SEXP r_max_q,
+    SEXP r_select_q,
+    SEXP r_n_path,
+    SEXP r_t_path,
+    SEXP r_penalties,
+    SEXP r_penalty_scales
+) {
 
     int nrx = Rf_nrows(r_x);
     int ncx = Rf_ncols(r_x);
