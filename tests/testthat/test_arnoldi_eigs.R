036240a1cd3d4ba23583249bbac357def9fc87fa
Christoph Rust <christoph.g.rust@gmail.com>
2026-02-27 17:17:50 +0100

    w7

--- tests/testthat/test_arnoldi_eigs.R
+++ tests/testthat/test_arnoldi_eigs.R
@@ -11,10 +11,13 @@ cat("sizeof(a_int) in C should match\n")
 test_that("Eigenvalue and -vector equality", {
   ## normalize evectors to be up to sign equivalent to result of eigen
   r2 <- eigen(spec)
+
+  cat("called eigen\n")
   gc()
+  cat("called eigen\n")
   r1 <- .Call("R_arnoldi_eigs", mat = spec, dim = dim, 4L, .Machine$double.eps, 1L, 1L, 0L, 0L)
   gc()
-  r2 <- eigen(spec)
+  cat("called eigs\n")
   diff_cp <- abs(Re(t(Conj(r1$vectors)) %*% r2$vectors[, 1:4])) - diag(4)
   expect_lte(sum(Re(r1$values - r2$values[1:4])^2), 1e-12)
   expect_lte(sum(diff_cp^2), 1e-12)
