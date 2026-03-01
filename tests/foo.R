3f92ff3157108503ff9761a6c4b1431fdc6054a3
Christoph Rust <christoph.g.rust@gmail.com>
2026-03-01 12:51:10 +0100

    wwwwww123

---
+++ tests/foo.R
@@ -0,0 +1,11 @@
+dim <- 150L
+m <- matrix(cos(runif(dim^2, -pi, pi)) + 1i * sin(runif(dim^2, -pi, pi)), ncol = dim)
+spec <- m %*% t(Conj(m))
+
+cat("TESTTEST:", .Machine$sizeof.long, "\n")
+cat("Integer size:", .Machine$sizeof.long, "\n")
+cat("sizeof(a_int) in C should match\n")
+
+cat("calling eigen\n")
+r2 <- eigen(spec)
+cat("called eigen\n")
